8057b1ed36f569b8c91b7da35f72d8e9a1a13c6d
Andrea Fraboni <151090404+AndreaFraboni@users.noreply.github.com>
2026-01-06 10:51:39 +0100

    starting study about exercize 3

--- Assets/_Project/Scripts/3DCODE/CubeBuilder.cs
+++ Assets/_Project/Scripts/3DCODE/CubeBuilder.cs
@@ -4,26 +4,26 @@ public class CubeBuilder : MonoBehaviour
 {
     [SerializeField] private GameObject CubePrefab;
 
-    [SerializeField] private int _row, _col, _depth;
+    [SerializeField] private int _cubesize;
     [SerializeField] private float _spacing;
 
     [SerializeField] private Vector3 _startPosition;
 
     private void Start()
     {
-        float startX = _startPosition.x - (_row / 2);
-        float startY = _startPosition.y - (_col / 2);
-        float startZ = _startPosition.z - (_depth / 2);
+        float startX = _startPosition.x - (_cubesize / 2);
+        float startY = _startPosition.y - (_cubesize / 2);
+        float startZ = _startPosition.z - (_cubesize / 2);
 
-        for (int x = 0; x < _row; x++)
+        for (int x = 0; x < _cubesize; x++)
         {
-            for (int y = 0; y < _col; y++)
+            for (int y = 0; y < _cubesize; y++)
             {
-                for (int z = 0; z < _depth; z++)
+                for (int z = 0; z < _cubesize; z++)
                 {
                     Vector3 position = new Vector3(startX + x * _spacing, startY + y * _spacing, startZ + z * _spacing);
 
-                    if (x == 0 || y == 0 || z == 0 || x == _row - 1 || y == _col - 1 || z == _depth - 1)
+                    if (x == 0 || y == 0 || z == 0 || x == _cubesize - 1 || y == _cubesize - 1 || z == _cubesize - 1)
                     {
                         GameObject cubematrix = Instantiate(CubePrefab, transform);
                         cubematrix.transform.position = position;
